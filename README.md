7bba2d48e49d3a4abea310fcceec62986acbb791
Sidney Bernardin <sidney.bernardin@gmail.com>
2026-05-02 18:25:13 -0400

    Fixed aws-secrets implementation.

--- README.md
+++ README.md
@@ -6,22 +6,7 @@
 go build -o secret-tunnel main.go
 
 # Run
-export SECRET_TUNNEL_POSTGRES_USERNAME="..."
-export SECRET_TUNNEL_POSTGRES_HOST="..."
-export SECRET_TUNNEL_POSTGRES_PORT="..."
-export SECRET_TUNNEL_POSTGRES_DATABASE="..."
 find ./src -name '*.yaml' | xargs ./secret-tunnel > out.yaml
-
-# Run with Docker
-docker build -t secret-tunnel ./Docker
-docker run \
-    -e SECRET_TUNNEL_POSTGRES_URL="..." \
-    -e SECRET_TUNNEL_POSTGRES_USERNAME="..." \
-    -e SECRET_TUNNEL_POSTGRES_HOST="..." \
-    -e SECRET_TUNNEL_POSTGRES_PORT="..." \
-    -e SECRET_TUNNEL_POSTGRES_DATABASE="..." \
-    -v ./local-src:/secret-tunnel/src \ # Mount your directory of input YAML files.
-    secret-tunnel > out.yaml
 ```
 
 ## Usage
@@ -30,13 +15,8 @@ docker run \
 ```
 
 ### Environment Variables
-* SECRET_TUNNEL_POSTGRES_USERNAME
-* SECRET_TUNNEL_POSTGRES_HOST
-* SECRET_TUNNEL_POSTGRES_PORT
-* SECRET_TUNNEL_POSTGRES_DATABSE
-* SELECT_TUNNEL_AWS_REGION
-* SELECT_TUNNEL_AWS_SECRET_POSTGRES_PASSWORD
-  > Your AWS secret-id of the postgres password.
+* ST_AWS_REGION
+* ST_AWS_DATABASE_SECRET_NAME
 
 ### Flags
 * -single-quote
--- main.go
+++ main.go
@@ -21,35 +21,28 @@ import (
 var (
 	singleQuote = flag.Bool("single-quote", false, "Use single/double quotes the output.")
 
-	awsRegion                   = os.Getenv("SELECT_TUNNEL_AWS_REGION")
-	awsPostgresPasswordSecretID = os.Getenv("SELECT_TUNNEL_AWS_SECRET_POSTGRES_PASSWORD")
-
-	pgUsername = os.Getenv("SECRET_TUNNEL_POSTGRES_USERNAME")
-	pgHost     = os.Getenv("SECRET_TUNNEL_POSTGRES_HOST")
-	pgPort     = os.Getenv("SECRET_TUNNEL_POSTGRES_PORT")
-	pgDatabase = os.Getenv("SECRET_TUNNEL_POSTGRES_DATABASE")
+	awsBaseEndpoint       = os.Getenv("ST_AWS_BASE_ENDPOINT") // Primarily for local testing.
+	awsDatabaseSecretName = os.Getenv("ST_AWS_DB_SECRET_NAME")
 )
 
 type (
-
-	// Sensor represents an input YAML file.
 	Sensor struct {
 		FullnameOverride string `json:"fullnameOverride"`
 		Configmap        struct {
-			Data sensorConfigmapData `json:"data"`
+			Data SensorConfigmapData `json:"data"`
 		} `json:"configmap"`
 		Secret struct {
-			Data sensorSecretData `json:"data"`
+			Data SensorSecretData `json:"data"`
 		} `json:"secret"`
 	}
 
-	sensorConfigmapData struct {
+	SensorConfigmapData struct {
 		StadiumDeviceEndpoint   *string `json:"STADIUM_DEVICE_ENDPOINT,omitempty"`
 		StadiumDeviceSensorUUID string  `yaml:"STADIUM_DEVICE_SENSOR_UUID" json:"-"`
 		StadiumDeviceType       string  `json:"STADIUM_DEVICE_TYPE,omitempty"`
 	}
 
-	sensorSecretData struct {
+	SensorSecretData struct {
 		StadiumDeviceAPIToken  *string `json:"STADIUM_DEVICE_API_TOKEN,omitempty"`
 		StadiumDeviceUsername  *string `json:"STADIUM_DEVICE_USERNAME,omitempty"`
 		StadiumDevicePassword  *string `json:"STADIUM_DEVICE_PASSWORD,omitempty"`
@@ -60,96 +53,147 @@ type (
 )
 
 type (
-
-	// Output represents the output YAML file.
 	Output struct {
-		Secrets []outputSecret `json:"secrets"`
+		Secrets []SensorSecret `json:"secrets"`
 	}
 
-	outputSecret struct {
+	SensorSecret struct {
 		Name    string `json:"name"`
 		Type    string `json:"type"`
 		KVPairs string `json:"kvpairs"`
 	}
 )
 
+var secretsManagerClient *secretsmanager.Client
+
 func main() {
 	flag.Parse()
 	ctx := context.Background()
 
-	postgresPassword, err := getAWSSecrets(ctx)
+	// Load AWS config and create a Secrets-Manager client.
+	awsConfig, err := config.LoadDefaultConfig(ctx)
 	if err != nil {
-		slog.Error("Cannot get AWS secrets", "err", err.Error())
-		return
+		slog.Error("cannot load AWS config", "err", err.Error())
 	}
 
-	// Get enabled sensor UUIDs.
-	enabledSensorUUIDs, err := getEnabledSensors(ctx, postgresPassword)
+	secretsManagerClient = secretsmanager.NewFromConfig(awsConfig,
+		func(o *secretsmanager.Options) {
+			if awsBaseEndpoint != "" {
+				o.BaseEndpoint = aws.String(awsBaseEndpoint)
+			}
+		})
+
+	// Get the UUIDs of only enabled sensors.
+	enabledSensorUUIDs, err := getEnabledSensors(ctx)
 	if err != nil {
 		slog.Error("Cannot get enabled sensor UUIDs", "err", err.Error())
 		return
 	}
 
-	var output Output
+	output := Output{
+		Secrets: make([]SensorSecret, 0, flag.NArg()),
+	}
 
-	// Process each file.
-	for _, filePath := range flag.Args() {
-		log := slog.With("file", filePath)
+	// Create a sensor-secret from each sensor-file.
+	for _, sensorFilePath := range flag.Args() {
+		log := slog.With("sensor_file", sensorFilePath)
 
-		// Open the file.
-		file, err := os.Open(filePath)
+		// Decode the sensor file.
+		sensor, err := decodeSensorFile(sensorFilePath)
 		if err != nil {
-			log.Error("Cannot open file", "err", err.Error())
+			log.Error("Cannot decode sensor-file", "err", err.Error())
 			return
 		}
 
-		// Read the file.
-		sensorYAML, err := io.ReadAll(file)
-		if err != nil {
-			log.Error("Cannot read file", "err", err.Error())
-			return
-		}
-
-		// YAML decode the file.
-		var sensor Sensor
-		if err := yaml.Unmarshal(sensorYAML, &sensor); err != nil {
-			log.Error("Cannot YAML decode file", "err", err.Error())
-			return
-		}
-
-		// Check if the sensor is enabled.
+		// Skip this sensor if it's disabled.
 		if !slices.Contains(enabledSensorUUIDs, sensor.Configmap.Data.StadiumDeviceSensorUUID) {
 			continue
 		}
 
-		// JSON encode the sensor's data objects.
-		kvpairsJSON, err := json.Marshal(struct {
-			sensorConfigmapData
-			sensorSecretData
-		}{sensor.Configmap.Data, sensor.Secret.Data})
-
+		// Create the sensor-secret.
+		sensorSecret, err := createSensorSecret(sensor)
 		if err != nil {
-			log.Error("Cannot JSON encode kvpairs", "err", err.Error())
+			log.Error("Cannot create sensor-secret", "err", err.Error())
 			return
 		}
 
-		// Add a new secret to output.
-		output.Secrets = append(output.Secrets, outputSecret{
-			Name:    sensor.FullnameOverride,
-			Type:    sensor.Configmap.Data.StadiumDeviceType,
-			KVPairs: string(kvpairsJSON),
-		})
+		output.Secrets = append(output.Secrets, *sensorSecret)
 	}
 
-	// YAML encode output into stdout.
+	// Encode the output into stdout.
 	if err := yaml.NewEncoder(os.Stdout, yaml.UseSingleQuote(*singleQuote)).Encode(output); err != nil {
-		slog.Error("Cannot YAML encode output", "err", err.Error())
+		slog.Error("Cannot encode output", "err", err.Error())
+		return
 	}
 }
 
+// decodeSensorFile returns the content of a sensor-file.
+func decodeSensorFile(sensorFilePath string) (*Sensor, error) {
+
+	// Open the sensor-file.
+	sensorFile, err := os.Open(sensorFilePath)
+	if err != nil {
+		return nil, errors.Wrap(err, "cannot open file")
+	}
+	defer sensorFile.Close()
+
+	// Read the sensor-file.
+	sensorYAML, err := io.ReadAll(sensorFile)
+	if err != nil {
+		return nil, errors.Wrap(err, "cannot read file")
+	}
+
+	// Decode the sensor-YAML.
+	var sensor Sensor
+	if err := yaml.Unmarshal(sensorYAML, &sensor); err != nil {
+		return nil, errors.Wrap(err, "cannot decode sensor")
+	}
+
+	return &sensor, nil
+}
+
+// createSensorSecret returns a new sensor-secret.
+func createSensorSecret(sensor *Sensor) (*SensorSecret, error) {
+
+	// Encode the sensor's data objects into one JSON object.
+	kvPairsJSON, err := json.Marshal(struct {
+		SensorConfigmapData
+		SensorSecretData
+	}{sensor.Configmap.Data, sensor.Secret.Data})
+
+	if err != nil {
+		return nil, errors.Wrap(err, "cannot encode kvpairs")
+	}
+
+	return &SensorSecret{
+		// Name:    sensor.FullnameOverride,
+		Name:    sensor.Configmap.Data.StadiumDeviceSensorUUID,
+		Type:    sensor.Configmap.Data.StadiumDeviceType,
+		KVPairs: string(kvPairsJSON),
+	}, nil
+}
+
 // getEnabledSensors returns the UUIDs of all enabled sensors from the database.
-func getEnabledSensors(ctx context.Context, postgresPassword string) ([]string, error) {
-	pgURL := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", pgUsername, postgresPassword, pgHost, pgPort, pgDatabase)
+func getEnabledSensors(ctx context.Context) ([]string, error) {
+
+	// Get the database-secret from Secrets-Manager.
+	secretResult, err := secretsManagerClient.GetSecretValue(ctx, &secretsmanager.GetSecretValueInput{SecretId: &awsDatabaseSecretName})
+	if err != nil {
+		return nil, errors.Wrap(err, "cannot get database-secret")
+	}
+
+	// Decode the database-secret.
+	var secret map[string]any
+	if err := json.Unmarshal([]byte(*secretResult.SecretString), &secret); err != nil {
+		return nil, errors.Wrap(err, "cannot decode database-secret")
+	}
+
+	pgURL := fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
+		secret["PG_USERNAME"],
+		secret["PG_PASSWORD"],
+		secret["PG_HOST"],
+		secret["PG_PORT"],
+		secret["PG_DATABASE"])
 
 	// Create connection to the database.
 	conn, err := pgx.Connect(ctx, pgURL)
@@ -158,7 +202,7 @@ func getEnabledSensors(ctx context.Context, postgresPassword string) ([]string, 
 	}
 	defer conn.Close(ctx)
 
-	// Get UUIDs of all enabled rows.
+	// Select all enabled sensors.
 	rows, err := conn.Query(ctx, `
 		SELECT sensor_uuid
 		FROM collections.sensors
@@ -171,39 +215,19 @@ func getEnabledSensors(ctx context.Context, postgresPassword string) ([]string, 
 
 	defer rows.Close()
 
-	// Add each row's UUID to a slice.
+	// Get each sensor's UUID.
 	uuids := []string{}
 	for rows.Next() {
 		var uuid string
 		if err := rows.Scan(&uuid); err != nil {
-			return nil, errors.Wrap(err, "cannot scan sensor")
+			return nil, errors.Wrap(err, "cannot scan database row")
 		}
 		uuids = append(uuids, uuid)
 	}
 
 	if rows.Err() != nil {
-		return nil, errors.Wrap(err, "cannot scan sensors")
+		return nil, errors.Wrap(err, "cannot scan database rows")
 	}
 
 	return uuids, nil
-}
-
-func getAWSSecrets(ctx context.Context) (postgresPassword string, err error) {
-
-	// Load AWS configuration.
-	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion(awsRegion))
-	if err != nil {
-		slog.Error("Cannot load AWS config", "err", err.Error())
-		return
-	}
-
-	secrets := secretsmanager.NewFromConfig(cfg)
-
-	// Get the postgres password.
-	postgresPasswordResult, err := secrets.GetSecretValue(ctx, &secretsmanager.GetSecretValueInput{SecretId: aws.String(awsPostgresPasswordSecretID)})
-	if err != nil {
-		return "", errors.Wrap(err, "cannot get postgres password from AWS")
-	}
-
-	return *postgresPasswordResult.SecretString, nil
 }
